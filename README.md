a4409e13e40932c4e10e7d5747556f7369b09b98
Dmitrii Ermizin <Www.ermik88@yandex.ru>
2023-10-29 23:22:51 +0300

    Первый коммит

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/MrSchneider1/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,26 @@
 // @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+const cardElement = cardTemplate.querySelector('.places__item');
+const cardImage = cardElement.querySelector('.card__image');
+let cardName = cardElement.querySelector('.card__title');
+
+function addCards() {
+    for (let i = 0; i < initialCards.length; i = i + 1) {
+        cardImage.src = initialCards[i].link;
+        cardName.textContent = initialCards[i].name;
+        let cardCopy = cardElement.cloneNode(true);
+        placesList.append(cardCopy);
+        const deleteButton = cardCopy.querySelector('.card__delete-button');
+        function deleteCard() {
+            const removedObject = deleteButton.closest('.places__item');
+            removedObject.remove();
+        }
+        deleteButton.addEventListener('click', deleteCard);
+    }
+}
+
+addCards();
 
 // @todo: DOM узлы
 
