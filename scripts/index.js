2772fa7491a1f5e36f7a1a5b7361effb6f6fed17
Dmitrii Ermizin <Www.ermik88@yandex.ru>
2023-11-01 22:08:44 +0300

    Исправил ошибки после 1 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,33 @@
 // @todo: Темплейт карточки
 const placesList = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
-const cardElement = cardTemplate.querySelector('.places__item');
-const cardImage = cardElement.querySelector('.card__image');
-let cardName = cardElement.querySelector('.card__title');
+
+function createCard(item) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardName = cardElement.querySelector('.card__title');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+
+    cardImage.src = item.link;
+    cardImage.alt = item.name;
+    cardName.textContent = item.name;
+    deleteButton.addEventListener('click', function() {
+        deleteCard(deleteButton);
+    });
+
+    return cardElement;
+}
+
+function deleteCard(item) {
+    const removedObject = item.closest('.places__item');
+    removedObject.remove();
+}
 
 function addCards() {
-    for (let i = 0; i < initialCards.length; i = i + 1) {
-        cardImage.src = initialCards[i].link;
-        cardName.textContent = initialCards[i].name;
-        let cardCopy = cardElement.cloneNode(true);
+    initialCards.forEach(function (item) {
+        const cardCopy = createCard(item);
         placesList.append(cardCopy);
-        const deleteButton = cardCopy.querySelector('.card__delete-button');
-        function deleteCard() {
-            const removedObject = deleteButton.closest('.places__item');
-            removedObject.remove();
-        }
-        deleteButton.addEventListener('click', deleteCard);
-    }
+    });
 }
 
 addCards();
